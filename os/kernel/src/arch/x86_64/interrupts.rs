09e6d95b63957d4e1d4183c2094889a32f35c2e6
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-09-11 06:09:03 +0630

    fix(os): make IDT initialization safe for Rust 2024

--- os/kernel/src/arch/x86_64/interrupts.rs
+++ os/kernel/src/arch/x86_64/interrupts.rs
@@ -1,29 +1,24 @@
-#![allow(dead_code)]
-
-use core::mem::size_of;
-use x86_64::structures::idt::InterruptDescriptorTable;
+use spin::Once;
 use x86_64::instructions::interrupts;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-static mut IDT: Option<InterruptDescriptorTable> = None;
+static IDT: Once<InterruptDescriptorTable> = Once::new();
 
-extern "x86-interrupt" fn breakpoint(_stack: x86_64::structures::idt::InterruptStackFrame) {}
+extern "x86-interrupt" fn breakpoint(_stack: InterruptStackFrame) {}
 
 extern "x86-interrupt" fn double_fault(
-    _stack: x86_64::structures::idt::InterruptStackFrame,
+    _stack: InterruptStackFrame,
     _error: u64,
 ) -> ! {
     loop { x86_64::instructions::hlt(); }
 }
 
 pub fn init() {
-    let mut idt = InterruptDescriptorTable::new();
-    idt.breakpoint.set_handler_fn(breakpoint);
-    unsafe {
+    IDT.call_once(|| {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint);
         idt.double_fault.set_handler_fn(double_fault);
-        IDT = Some(idt);
-        IDT.as_ref().unwrap().load();
-    }
+        idt
+    }).load();
     interrupts::enable();
 }
-
-pub const IDT_ENTRY_SIZE: usize = size_of::<x86_64::structures::idt::Entry<()>>();
