c86ff3c22a7872ce969e3e92781e8400228ec7c2
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-09-11 19:30:53 +0630

    fix(os): add package target required by Cargo build

---
+++ os/src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn main() -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
