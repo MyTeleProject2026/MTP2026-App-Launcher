aa0730e3c0d669a1f322c1541045fac7e830aa29
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-09-15 03:28:56 +0630

    feat: load universal MTP2026 runtime styles

--- frontend/src/nativeRuntime.js
+++ frontend/src/nativeRuntime.js
@@ -22,6 +22,7 @@ import './mtp2026GuestProfiles.js';
 import './mtp2026GuestShell.js';
 import './mtp2026GuestShell.css';
 import './mtp2026UniversalOS.js';
+import './mtp2026UniversalOS.css';
 
 export function getNativeCapabilities() {
   return nativeCapabilities();
