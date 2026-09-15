f215dae232526596140a6f792f6acdd2225938dd
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-09-15 08:02:32 +0630

    feat: wire shared VexaAccount SSO into MTP2026 runtime

--- frontend/src/nativeRuntime.js
+++ frontend/src/nativeRuntime.js
@@ -23,6 +23,7 @@ import './mtp2026GuestShell.js';
 import './mtp2026GuestShell.css';
 import './mtp2026UniversalOS.js';
 import './mtp2026UniversalOS.css';
+import './mtp2026VexaAccountSSO.js';
 
 export function getNativeCapabilities() {
   return nativeCapabilities();
